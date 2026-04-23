1020db61f76f70171612863c1ac61193c09b1b29
adi <aditya@adytya.local>
2026-04-23 12:16:34 +0530

    new pyth

--- storage.py
+++ storage.py
@@ -13,3 +13,9 @@ def load_from_file():
             return json.load(f)
     except FileNotFoundError:
         return []
+ def save_to_file(tasks):
+    """Saves the current task list to a JSON file."""
+    with open('tasks_db.json', 'w') as f:
+        json.dump(tasks, f)
+    print("...System: Data persisted successfully.")
+   
